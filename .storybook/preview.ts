ee582bb96afba8603beda633ef823c23df771830
mykeels <mykehell123@gmail.com>
2025-03-02 21:09:22 -0500

    chore: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import "../src/index.css";
+
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [],
+};
+
+export default preview;
