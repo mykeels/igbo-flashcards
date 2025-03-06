d598e63cd4ed07029ee526741066551062878e26
mykeels <mykehell123@gmail.com>
2025-03-05 19:51:05 -0500

    refac: Examples

--- src/components/WordFlashCard.tsx
+++ src/components/WordFlashCard.tsx
@@ -4,6 +4,7 @@ import { clsx } from "clsx";
 import { createRootRoute } from "@tanstack/react-router";
 import words from "../data/words.json";
 import { useMouseHold } from "@/hooks/useMouseHold.hook";
+import { Examples } from "./Examples";
 
 // Define the WordCard component
 const WordFlashCard: React.FC = () => {
@@ -95,35 +96,3 @@ export default WordFlashCard;
 export const Route = createRootRoute({
   component: WordFlashCard,
 });
-
-export const Examples = ({
-  examples,
-  backgroundColor,
-}: {
-  examples: {
-    english: string;
-    igbo: string;
-  }[];
-  backgroundColor: string;
-}) => {
-  const [showMore, setShowMore] = useState(false);
-  const toggleShowMore = () => {
-    setShowMore(!showMore);
-  };
-  return (
-    <div
-      className="flex flex-col gap-2 text-black font-semibold text-center"
-      onClick={toggleShowMore}
-    >
-      {examples.slice(0, showMore ? examples.length : 1).map((example) => (
-        <div
-          key={example.english}
-          className={clsx("px-4 py-2 border border-black", backgroundColor)}
-        >
-          <p>{example.english}</p>
-          <p>{example.igbo}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
